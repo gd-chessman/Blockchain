3fc63a90228c5779bc7b6c70125d3f8f9949a1c9
Le Van Quy <vannn.quy@gmail.com>
2025-04-01 16:10:28 +0700

    up

--- src/app/trading/page.tsx
+++ src/app/trading/page.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { t } from "@/lang"
+import { useRouter } from "next/navigation";
 
 
 const tokens = [
@@ -12,7 +13,6 @@ const tokens = [
     address: 'BBbk8Msyp8viNcZcWjSJc6rPGj1DKXaT72dktdtMpump',
     decimals: 18,
     verified: 'Yes',
-    actions: ['Buy']
   },
   {
     name: 'ARCHIE',
@@ -20,12 +20,13 @@ const tokens = [
     address: 'BBbk8Msyp8viNcZcWjSJc6rPGj1DKXaT72dktdtMpump',
     decimals: 18,
     verified: 'Yes',
-    actions: ['Buy']
   },
   // Thêm các dữ liệu token khác nếu cần
 ];
 
 export default function Trading() {
+
+  const router = useRouter()
 
   return (
     <div className="container mx-auto p-6">
@@ -39,17 +40,17 @@ export default function Trading() {
             <Table>
               <TableHeader>
                 <TableRow className="bg-muted/50">
-                  <TableHead>Token</TableHead>
-                  <TableHead>Symbol</TableHead>
-                  <TableHead>Address</TableHead>
-                  <TableHead>Decimals</TableHead>
-                  <TableHead>Verified</TableHead>
-                  <TableHead>Action</TableHead>
+                  <TableHead>{t('trading.token')}</TableHead>
+                  <TableHead>{t('trading.symbol')}</TableHead>
+                  <TableHead>{t('trading.address')}</TableHead>
+                  <TableHead>{t('trading.decimals')}</TableHead>
+                  <TableHead>{t('trading.verified')}</TableHead>
+                  <TableHead>{t('trading.action')}</TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
                 {tokens.map((token, index) => (
-                  <TableRow key={index} className="hover:bg-muted/30">
+                  <TableRow key={index} className="hover:bg-muted/30 cursor-pointer" onClick={()=> router.push('trading/token')}>
                     <TableCell>
                       <div className="flex items-center gap-2">
                         <img src="https://d1hjkbq40fs2x4.cloudfront.net/2016-01-31/files/1045-2.jpg" alt="" className="size-10 rounded-full" />
@@ -62,16 +63,16 @@ export default function Trading() {
                     <TableCell>{token.verified}</TableCell>
                     <TableCell>
                       <div className="flex space-x-2">
-                        {token.actions.map((action, i) => (
+                       
                           <Button
-                            key={i}
+        
                             variant="outline"
                             size="sm"
                             className="bg-blue-50 dark:bg-blue-900/20 text-green-600 dark:text-green-300 border-green-200 dark:border-green-800 hover:bg-green-100 dark:hover:bg-green-900/30"
                           >
-                            {action}
+                            {t('trading.buy')}
                           </Button>
-                        ))}
+      
                       </div>
                     </TableCell>
                   </TableRow>
