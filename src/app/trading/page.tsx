d223c5fb5c99947afe91e60d649165f0839f6e32
Le Van Quy <vannn.quy@gmail.com>
2025-04-03 16:41:48 +0700

    up

--- src/app/trading/page.tsx
+++ src/app/trading/page.tsx
@@ -1,18 +1,34 @@
 "use client";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow,} from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { t } from "@/lang";
 import { useRouter } from "next/navigation";
 import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { useState, useEffect } from "react";
 import { useWebSocket } from "@/hooks/useWebSocket";
+import { SolonaTokenService } from "@/services/api";
 
 export default function Trading() {
   const [searchQuery, setSearchQuery] = useState("");
   const { messages } = useWebSocket();
-  const [tokens, setTokens] = useState<{ name: string; address: string; symbol: string; decimals: number; isVerified: boolean }[]>([]);
+  const [tokens, setTokens] = useState<
+    {
+      name: string;
+      address: string;
+      symbol: string;
+      decimals: number;
+      isVerified: boolean;
+    }[]
+  >([]);
   const router = useRouter();
 
   // Parse messages and extract tokens
@@ -31,12 +47,11 @@ export default function Trading() {
     }
   }, [messages]); // Runs every time messages change
 
-  // Filter tokens based on search query
-  const filteredTokens = tokens.filter(
-    (token) =>
-      token.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      token.address.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const searchData = async()=>{
+    const res = await SolonaTokenService.getSearchTokenInfor(searchQuery)
+    console.log(res)
+  }
+
 
   return (
     <div className="container mx-auto p-6">
@@ -44,7 +59,7 @@ export default function Trading() {
         <CardHeader className="flex justify-between flex-row items-center">
           <CardTitle>{t("trading.list_token_title")}</CardTitle>
           <div className="relative w-full md:w-auto mt-4 md:mt-0">
-            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
+            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" onClick={()=> searchData()}/>
             <Input
               placeholder={"Search by token name or address"}
               className="pl-10 w-full md:w-[300px]"
@@ -53,57 +68,61 @@ export default function Trading() {
             />
           </div>
         </CardHeader>
-        <CardContent>
-          <div className="rounded-lg overflow-hidden">
-            <Table>
-              <TableHeader>
-                <TableRow className="bg-muted/50">
-                  <TableHead>{t("trading.token")}</TableHead>
-                  <TableHead>{t("trading.symbol")}</TableHead>
-                  <TableHead>{t("trading.address")}</TableHead>
-                  <TableHead>{t("trading.decimals")}</TableHead>
-                  <TableHead>{t("trading.verified")}</TableHead>
-                  <TableHead>{t("trading.action")}</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {filteredTokens.map((token, index) => (
-                  <TableRow
-                    key={index}
-                    className="hover:bg-muted/30 cursor-pointer"
-                    onClick={() => router.push(`trading/token?address=${token.address}`)}
-                  >
-                    <TableCell>
-                      <div className="flex items-center gap-2">
-                        <img
-                          src="https://d1hjkbq40fs2x4.cloudfront.net/2016-01-31/files/1045-2.jpg"
-                          alt="token logo"
-                          className="size-10 rounded-full"
-                        />
-                        <p>{token.name}</p>
-                      </div>
-                    </TableCell>
-                    <TableCell>{token.symbol}</TableCell>
-                    <TableCell>{token.address}</TableCell>
-                    <TableCell>{token.decimals}</TableCell>
-                    <TableCell>{token.isVerified ? "Yes" : "No"}</TableCell>
-                    <TableCell>
-                      <div className="flex space-x-2">
-                        <Button
-                          variant="outline"
-                          size="sm"
-                          className="bg-blue-50 dark:bg-blue-900/20 text-green-600 dark:text-green-300 border-green-200 dark:border-green-800 hover:bg-green-100 dark:hover:bg-green-900/30"
-                        >
-                          {t("trading.buy")}
-                        </Button>
-                      </div>
-                    </TableCell>
+        {tokens && (
+          <CardContent>
+            <div className="rounded-lg overflow-hidden">
+              <Table>
+                <TableHeader>
+                  <TableRow className="bg-muted/50">
+                    <TableHead>{t("trading.token")}</TableHead>
+                    <TableHead>{t("trading.symbol")}</TableHead>
+                    <TableHead>{t("trading.address")}</TableHead>
+                    <TableHead>{t("trading.decimals")}</TableHead>
+                    <TableHead>{t("trading.verified")}</TableHead>
+                    <TableHead>{t("trading.action")}</TableHead>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </div>
-        </CardContent>
+                </TableHeader>
+                <TableBody>
+                  {tokens.map((token, index) => (
+                    <TableRow
+                      key={index}
+                      className="hover:bg-muted/30 cursor-pointer"
+                      onClick={() =>
+                        router.push(`trading/token?address=${token.address}`)
+                      }
+                    >
+                      <TableCell>
+                        <div className="flex items-center gap-2">
+                          <img
+                            src="https://d1hjkbq40fs2x4.cloudfront.net/2016-01-31/files/1045-2.jpg"
+                            alt="token logo"
+                            className="size-10 rounded-full"
+                          />
+                          <p>{token.name}</p>
+                        </div>
+                      </TableCell>
+                      <TableCell>{token.symbol}</TableCell>
+                      <TableCell>{token.address}</TableCell>
+                      <TableCell>{token.decimals}</TableCell>
+                      <TableCell>{token.isVerified ? "Yes" : "No"}</TableCell>
+                      <TableCell>
+                        <div className="flex space-x-2">
+                          <Button
+                            variant="outline"
+                            size="sm"
+                            className="bg-blue-50 dark:bg-blue-900/20 text-green-600 dark:text-green-300 border-green-200 dark:border-green-800 hover:bg-green-100 dark:hover:bg-green-900/30"
+                          >
+                            {t("trading.buy")}
+                          </Button>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
+          </CardContent>
+        )}
       </Card>
     </div>
   );
