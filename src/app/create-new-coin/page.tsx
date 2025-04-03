ce12ae38e9d82e5918636bbcb3bd4ef327ecb9ad
Le Van Quy <vannn.quy@gmail.com>
2025-04-03 12:23:58 +0700

    up

--- src/app/create-new-coin/page.tsx
+++ src/app/create-new-coin/page.tsx
@@ -36,6 +36,7 @@ export default function CreateCoin() {
   const { data: memeCoins } = useQuery({
     queryKey: ['private-keys'],
     queryFn: getMyTokens,
+    placeholderData: [],
   });
   
   const { 
