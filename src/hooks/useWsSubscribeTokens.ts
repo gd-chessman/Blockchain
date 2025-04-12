96df8dc1a2d1b5b6f84f47beb3a9e421f520f98b
Le Van Quy <vannn.quy@gmail.com>
2025-04-12 20:15:53 +0700

    up

--- src/hooks/useWsSubscribeTokens.ts
+++ src/hooks/useWsSubscribeTokens.ts
@@ -35,7 +35,7 @@ export function useWsSubscribeTokens(params?: SubscribeParams) {
   const updateIntervalRef = useRef<NodeJS.Timeout | null>(null);
   const isInitialLoadRef = useRef(true);
   const MAX_STACK_SIZE = 100; // Giới hạn stack 100 token
-  const DISPLAY_INTERVAL = 1000; // Hiển thị mỗi s
+  const DISPLAY_INTERVAL = 500; // Hiển thị mỗi s
   const TOKENS_PER_UPDATE = 1; // Số token cập nhật mỗi lần
 
   const convertToken = (token: any): Token => {
