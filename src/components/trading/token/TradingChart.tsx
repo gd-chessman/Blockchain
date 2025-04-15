9f2f401dc874a229cd6d30553df019dbc86ada83
Le Van Quy <vannn.quy@gmail.com>
2025-04-15 17:17:30 +0700

    up

--- src/components/trading/token/TradingChart.tsx
+++ src/components/trading/token/TradingChart.tsx
@@ -1,48 +1,108 @@
-import IframeChartPage from '@/components/chart/IframeChartPage';
-import { useLang } from '@/lang/useLang';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/ui/card'
-import React from 'react'
+import IframeChartPage from "@/components/chart/IframeChartPage";
+import { useLang } from "@/lang/useLang";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/ui/card";
+import { truncateString } from "@/utils/format";
+import React from "react";
+import { Copy } from "lucide-react";
+import { ToastNotification } from "@/ui/toast";
+import { useState } from "react";
 
-export default function TradingChart({ tokenInfor, address }: { tokenInfor: any, address: any }) {
+export default function TradingChart({
+  tokenInfor,
+  address,
+}: {
+  tokenInfor: any;
+  address: any;
+}) {
   const { t } = useLang();
+  const [showToast, setShowToast] = useState(false);
+
+  const handleCopyAddress = () => {
+    navigator.clipboard.writeText(tokenInfor?.address);
+    setShowToast(true);
+  };
+
   return (
-    <Card className="mb-6 shadow-md dark:shadow-blue-900/5 border lg:col-span-2">
-    <CardHeader>
-      <div className="flex items-center justify-between flex-wrap">
-        <div>
-          <CardTitle>{tokenInfor?.symbol}/SOL</CardTitle>
-          <CardDescription>{tokenInfor?.name}</CardDescription>
-        </div>
-        <div className="text-right">
-          <div className="text-sm flex gap-4 ">
-            <div className="flex items-center flex-col">
-              <span>{t("trading.tokenInfo.marketCap")}:</span>
-              <span className="text-muted-foreground">${tokenInfor?.marketCap?.toFixed(1)}K</span>
+    <>
+      {showToast && (
+        <ToastNotification
+          message={t("createCoin.copySuccess")}
+          duration={2000}
+          onClose={() => setShowToast(false)}
+        />
+      )}
+      <Card className="mb-6 shadow-md dark:shadow-blue-900/5 border lg:col-span-2">
+        <CardHeader>
+          <div className="flex items-center justify-between flex-wrap gap-4">
+            <div className="flex items-center gap-2">
+              <img
+                src={tokenInfor?.logoUrl || "/placeholder.png"}
+                alt={tokenInfor?.symbol}
+                className="size-12 rounded-full"
+              />
+              <div>
+                <div className="flex items-center gap-2">
+                  <CardTitle>{tokenInfor?.symbol}/SOL</CardTitle>
+                  <CardDescription>{tokenInfor?.name}</CardDescription>
+                </div>
+                <div className="flex items-center gap-2">
+                  <p className="text-sm">
+                    {truncateString(tokenInfor?.address, 10)}
+                  </p>
+                  <button
+                    onClick={handleCopyAddress}
+                    className="p-1 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-md"
+                  >
+                    <Copy className="h-4 w-4" />
+                  </button>
+                </div>
+              </div>
             </div>
 
-            <div className="flex items-center flex-col">
-              <span>{t("trading.tokenInfo.volume24h")}:</span>
-              <span className="text-muted-foreground">${tokenInfor?.volume24h?.toFixed(1)}K</span>
+            <div className="text-right">
+              <div className="text-sm flex gap-4 ">
+                <div className="flex items-center flex-col">
+                  <span>{t("trading.tokenInfo.marketCap")}:</span>
+                  <span className="text-muted-foreground">
+                    ${tokenInfor?.marketCap?.toFixed(1)}K
+                  </span>
+                </div>
+
+                <div className="flex items-center flex-col">
+                  <span>{t("trading.tokenInfo.volume24h")}:</span>
+                  <span className="text-muted-foreground">
+                    ${tokenInfor?.volume24h?.toFixed(1)}K
+                  </span>
+                </div>
+                <div className="flex items-center flex-col">
+                  <span>{t("trading.tokenInfo.liquidity")}:</span>
+                  <span className="text-muted-foreground">
+                    ${tokenInfor?.liquidity?.toFixed(1)}K
+                  </span>
+                </div>
+                <div className="flex items-center flex-col">
+                  <span>{t("trading.tokenInfo.holders")}:</span>
+                  <span className="text-muted-foreground">
+                    {tokenInfor?.holders}
+                  </span>
+                </div>
+              </div>
+              <div className="text-2xl font-bold">
+                {/* ${tokenPrice?.priceUSD?.toFixed(9) || "0.00"} */}
+              </div>
             </div>
-            <div className="flex items-center flex-col">
-              <span>{t("trading.tokenInfo.liquidity")}:</span>
-              <span className="text-muted-foreground">${tokenInfor?.liquidity?.toFixed(1)}K</span>
-            </div>
-            <div className="flex items-center flex-col">
-              <span>{t("trading.tokenInfo.holders")}:</span>
-              <span className="text-muted-foreground">{tokenInfor?.holders}</span>
-            </div>
-            
           </div>
-          <div className="text-2xl font-bold">
-            {/* ${tokenPrice?.priceUSD?.toFixed(9) || "0.00"} */}
-          </div>
-        </div>
-      </div>
-    </CardHeader>
-    <CardContent>
-      <IframeChartPage token={address} />
-    </CardContent>
-  </Card>
-  )
+        </CardHeader>
+        <CardContent>
+          <IframeChartPage token={address} />
+        </CardContent>
+      </Card>
+    </>
+  );
 }
