7a78e8edfb9225e9adb2779fbbaa34ad9d879732
Le Van Quy <vannn.quy@gmail.com>
2025-04-18 09:50:36 +0700

    up

--- src/components/trading/token/OtherCoins.tsx
+++ src/components/trading/token/OtherCoins.tsx
@@ -210,8 +210,8 @@ export default function OtherCoins() {
       <CardContent>
         <div className="grid grid-cols-1 gap-4">
           <div className="flex items-center gap-4 mb-4">
-            <div className="flex gap-2 flex-1">
-              <div className="flex border rounded-md overflow-hidden flex-1">
+            <div className="flex gap-2 flex-1 overflow-x-auto [&::-webkit-scrollbar]:h-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 dark:[&::-webkit-scrollbar-thumb]:bg-gray-600 [&::-webkit-scrollbar-track]:bg-transparent">
+              <div className="flex border rounded-md overflow-hidden flex-1 min-w-max">
                 {sortOptions.map((option, index) => (
                   <Button
                     key={option.value}
