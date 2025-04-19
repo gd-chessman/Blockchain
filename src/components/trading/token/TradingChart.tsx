93b7099572445c42441723d5b4cdefd92514d55a
Le Van Quy <vannn.quy@gmail.com>
2025-04-19 09:34:11 +0700

    up

--- src/components/trading/token/TradingChart.tsx
+++ src/components/trading/token/TradingChart.tsx
@@ -52,7 +52,9 @@ export default function TradingChart({
               />
               <div>
                 <div className="flex items-center gap-2">
-                  <CardTitle className="text-xl">{tokenInfor?.symbol}/SOL</CardTitle>
+                  <CardTitle className="text-xl">
+                    {tokenInfor?.symbol}/SOL
+                  </CardTitle>
                   <CardDescription>{tokenInfor?.name}</CardDescription>
                   {tokenInfor?.program === "pumpfun" && (
                     <img src="/pump.webp" alt="pump" className="h-6 w-6" />
@@ -74,7 +76,16 @@ export default function TradingChart({
                     rel="noopener noreferrer"
                     className="p-1 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-md"
                   >
-                    <Search className="h-4 w-4" />
+                    <svg
+                      width="14"
+                      height="14"
+                      version="1.1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 300 300"
+                    >
+                      <path d="M178.57 127.15 290.27 0h-26.46l-97.03 110.38L89.34 0H0l117.13 166.93L0 300.25h26.46l102.4-116.59 81.8 116.59h89.34M36.01 19.54H76.66l187.13 262.13h-40.66" />
+                    </svg>
                   </a>
                   {tokenInfor?.telegram && (
                     <a
