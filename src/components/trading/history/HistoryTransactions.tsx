d58400fc78a32385e2d748251ecc3642d5d38685
Le Van Quy <vannn.quy@gmail.com>
2025-04-19 16:20:49 +0700

    up

--- src/components/trading/history/HistoryTransactions.tsx
+++ src/components/trading/history/HistoryTransactions.tsx
@@ -58,44 +58,44 @@ export default function HistoryTransactions({ orders = [], historyTransactionsRe
     );
 
     return (
-        <Card className="shadow-md dark:shadow-blue-900/5 border h-full">
-            <CardContent>
-                <div ref={historyTransactionsRef} className={`overflow-x-auto max-h-[80svh] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 ${className}`}>
+        <Card className="shadow-md dark:shadow-blue-900/5 border h-full ">
+            <CardContent className="px-0 ">
+                <div ref={historyTransactionsRef} className={`overflow-x-auto max-h-[80svh] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 rounded-lg ${className} `}>
                     <table className="w-full">
-                        <thead>
+                        <thead >
                             <tr className="text-sm text-muted-foreground border-b sticky top-0 bg-background">
-                                <th className="text-left py-3 px-1">{t("trading.time")}</th>
-                                <th className="text-left py-3 px-1 whitespace-nowrap">{t("trading.type")}</th>
-                                <th className="text-left py-3 px-1">{t("trading.price")}</th>
-                                <th className="text-left py-3 px-1">{t("trading.amount")}</th>
-                                <th className="text-left py-3 px-1">{t("trading.total")}</th>
-                                <th className="text-left py-3 px-1">{t("trading.status")}</th>
-                                <th className="text-left py-3 px-1">{t("trading.address")}</th>
+                                <th className="text-left py-3 px-2">{t("trading.time")}</th>
+                                <th className="text-left py-3 px-2 whitespace-nowrap">{t("trading.type")}</th>
+                                <th className="text-left py-3 px-2">{t("trading.price")}</th>
+                                <th className="text-left py-3 px-2">{t("trading.amount")}</th>
+                                <th className="text-left py-3 px-2">{t("trading.total")}</th>
+                                <th className="text-left py-3 px-2">{t("trading.status")}</th>
+                                <th className="text-left py-3 px-2">{t("trading.address")}</th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody >
                             {orders.length === 0 ? (
                                 Array(5).fill(0).map((_, index) => (
                                     <tr key={index} className="text-sm border-b">
-                                        <td className="py-3 px-1">
+                                        <td className="py-3 px-2">
                                             <Skeleton className="h-4 w-24" />
                                         </td>
-                                        <td className="py-3 px-1">
+                                        <td className="py-3 px-2">
                                             <Skeleton className="h-4 w-16" />
                                         </td>
-                                        <td className="py-3 px-1">
+                                        <td className="py-3 px-2">
                                             <Skeleton className="h-4 w-20" />
                                         </td>
-                                        <td className="py-3 px-1">
+                                        <td className="py-3 px-2">
                                             <Skeleton className="h-4 w-16" />
                                         </td>
-                                        <td className="py-3 px-1">
+                                        <td className="py-3 px-2">
                                             <Skeleton className="h-4 w-20" />
                                         </td>
-                                        <td className="py-3 px-1">
+                                        <td className="py-3 px-2">
                                             <Skeleton className="h-4 w-16" />
                                         </td>
-                                        <td className="py-3 px-1">
+                                        <td className="py-3 px-2">
                                             <Skeleton className="h-4 w-24" />
                                         </td>
                                     </tr>
@@ -104,10 +104,10 @@ export default function HistoryTransactions({ orders = [], historyTransactionsRe
                                 filteredOrders.map(
                                     (order: Transaction, index: number) => (
                                         <tr key={index} className="text-sm border-b">
-                                            <td className="py-3 px-1">
+                                            <td className="py-3 px-2">
                                                 {formatTime(order.blockUnixTime || order.block_unix_time)}
                                             </td>
-                                            <td className="py-3 px-1">
+                                            <td className="py-3 px-2">
                                                 <span
                                                     className={
                                                         order.side === "buy"
@@ -118,21 +118,21 @@ export default function HistoryTransactions({ orders = [], historyTransactionsRe
                                                     {t(`trading.${order.side}`)}
                                                 </span>
                                             </td>
-                                            <td className="py-3 px-1">
+                                            <td className="py-3 px-2">
                                                 ${formatPrice(order.from.price || order.from.nearestPrice)}
                                             </td>
-                                            <td className="py-3 px-1">
+                                            <td className="py-3 px-2">
                                                 {formatNumberWithSuffix(order.from.ui_amount)}
                                             </td>
-                                            <td className="py-3 px-1">
+                                            <td className="py-3 px-2">
                                                 ${formatVolume(order.volumeUSD || (order.from.ui_amount * order.from.price))}
                                             </td>
-                                            <td className="py-3 px-1 uppercase">
+                                            <td className="py-3 px-2 uppercase">
                                                 <span className="text-blue-600 whitespace-nowrap">
                                                     {t("trading.completed")}
                                                 </span>
                                             </td>
-                                            <td className="py-3 px-1">
+                                            <td className="py-3 px-2">
                                                 <div className="flex items-center gap-1">
                                                     {truncateString(order.owner, 8)}
                                                     <button 
